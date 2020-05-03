f8535749ed3b11b7c2e5d4aa775f7eae17682ee0
WuYiyang <wuyiyang17@mails.tsinghua.edu.cn>
2020-05-03 12:27:38 +0800

    fit std and ParticleTruth

--- thres.py
+++ thres.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import argparse
-psr = argparse.ArgumentParser()
-psr.add_argument('-o', dest='opt', type=str, help='output file')
-psr.add_argument('ipt', type=str, help='input file')
-psr.add_argument('--met', type=str, help='fitting method')
-args = psr.parse_args()
-
 import numpy as np
 import tables
 from tqdm import tqdm
-from DataIO import ReadPETruth
+from DataIO import ReadPETruth, ReadParticleType
+from time import time
+from multiprocessing import Pool
+from IPython import embed
+from Grader import calAUC
 
 
 class AnswerData(tables.IsDescription) :
@@ -18,32 +15,50 @@ class AnswerData(tables.IsDescription) :
     Alpha = tables.Float32Col(pos=1)
 
 
-def StdMethod(PETruth, AnswerRow) :
+def CalulateStd(PETruth) :
     e_ans, i_ans = np.unique(PETruth['EventID'], return_index=True)
     i_ans = np.append(i_ans, len(PETruth))
+    STD = np.empty(len(e_ans), dtype=np.float32)
     for i in tqdm(range(len(e_ans))) :
         this_event_petruth = PETruth[i_ans[i]:i_ans[i + 1]]
-        channelid, channel_index = np.unique(this_event_petruth['ChannelID'], return_index=True)
-        channel_index = np.append(channel_index, len(this_event_petruth))
-        this_event_variation = 0
-        nFiredPMTs = len(channelid)
-        for j in range(nFiredPMTs) :
-            this_channel_petruth = this_event_petruth[channel_index[j]:channel_index[j + 1]]
-            this_event_variation += np.var(this_channel_petruth["PETime"])
-        this_event_answer = min(max((np.sqrt(this_event_variation / nFiredPMTs) - 24) / 8, 0), 1)
-        AnswerRow['EventID'] = e_ans[i]
-        AnswerRow['Alpha'] = this_event_answer
-        AnswerRow.append()
+        this_event_answer = np.std(this_event_petruth["PETime"])
+        STD[i] = this_event_answer
+    return {"STD": STD, "EventID": e_ans}
 
 
-def main(fopt, fipt, method):
+def ProcessTrainFile(filename) :
+    PETruth = ReadPETruth(filename)["Data"]
+    ParticleType = ReadParticleType(filename)
+    return {"STD": CalulateStd(PETruth)["STD"], "ParticleType": ParticleType}
+
+
+def train() :
+    filenames = ["dataset/pre-{}.h5".format(i) for i in range(10)]
+    with Pool(len(filenames)) as pool :
+        Result = pool.map(ProcessTrainFile, filenames)
+    STD = np.concatenate([result["STD"] for result in Result])
+    ParticleType = np.concatenate([result["ParticleType"] for result in Result])
+    return np.polyfit(STD, ParticleType, deg=3)
+
+
+def main(fopt, fipt, para):
     PETruth = ReadPETruth(fipt)["Data"]
     AnswerFile = tables.open_file(fopt, mode="w", title="AlphaBeta", filters=tables.Filters(complevel=4))
     AnswerTable = AnswerFile.create_table("/", "Answer", AnswerData, "Answer")
-    StdMethod(PETruth, AnswerTable.row)
+    classifier = np.poly1d(para)
+    STD = CalulateStd(PETruth)
+    Answer = classifier(STD["STD"])
+    AnswerTable.append(list(zip(STD["EventID"], Answer)))
     AnswerTable.flush()
     AnswerFile.close()
 
 
 if __name__ == '__main__':
-    main(args.opt, args.ipt, args.met)
+    import argparse
+    psr = argparse.ArgumentParser()
+    psr.add_argument('-o', dest='opt', type=str, help='output file')
+    psr.add_argument('ipt', type=str, help='input file')
+    psr.add_argument('--met', type=str, help='fitting method')
+    args = psr.parse_args()
+    para = train()
+    main(args.opt, args.ipt, para)
